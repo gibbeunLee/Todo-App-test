3998b27e3a87236c5b317a2c4bbced2b4ecb37b4
gibbeunLee <gibbeunlee@gmail.com>
2023-06-12 17:38:31 +0900

    init

---
+++ Todo-app-test/src/test/java/com/codestates/Todoapptest/TodoAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.Todoapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
